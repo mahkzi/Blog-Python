c33ba2cda6ccff2ea8c218af1b53a66d3ee7c73f
Castro Máximo <maximocastrobraga@gmail.com>
2026-04-09 15:25:47 -0300

    Base de modelos, todo el contenido listo para entrega

--- Publicaciones/urls.py
+++ Publicaciones/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from Publicaciones.views import home
+from Publicaciones.views import home, blog
 
 urlpatterns = [
      path("", home),
+     path("blog/", blog)
 ]
--- Publicaciones/views.py
+++ Publicaciones/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,"home.html",{})
+    return render(request,"publicaciones/home.html",{})
+def blog(request):
+    return render(request,"publicaciones/blog.html",{})
+
